bac50696cbf0249c49c1a4f4886c3408c1b293af
Richard3332@gmail.com <Richard3332@gmail.com@e0199c09-a546-0725-641d-050fd65334ba>
2011-06-20 09:50:25 +0000

    New dataView and dataView is changed to Default

--- trunk/Car2CloudWebApp/Car2CloudWebApp/Default.aspx
+++ trunk/Car2CloudWebApp/Car2CloudWebApp/Default.aspx
@@ -1,19 +1,67 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="Site.master" AutoEventWireup="true"
-    CodeBehind="Default.aspx.cs" Inherits="ictlab._Default" EnableEventValidation="false"%>
+﻿<%@ Page Title="Overzicht" Language="C#" MasterPageFile="Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="ictlab.Default" %>
 
-<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
-
+<%@ Register Assembly="WebChart" Namespace="WebChart" TagPrefix="Web" %>
 <asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript">
+    //<![CDATA[
+        function loadScript() {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = "http://maps.google.com/maps/api/js?sensor=false&callback=initialize";
+            document.body.appendChild(script);
+        }
+
+        window.onload = loadScript;
+    //]]>
+    </script>
+
+<!--Literal Control for runtime javascript-->
+<asp:Literal ID="js" runat="server"></asp:Literal>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="LoginContent" runat="server">
     <a href="Uitloggen.aspx">Uitloggen</a>
 </asp:Content>
 <asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
-    <div class="titel2">Datastore</div>
-    <img id="line" src="Images/loginLine.gif" width="870" height="2" alt="" />
-    <p class="ptext">De hieronderstaande tabel bevat alle data die vanuit de auto naar de cloud zijn geupload.</p>
-    <br /><br /><br />
-    <telerik:RadGrid ID="Grd1" runat="server">
-        <PagerStyle Mode="NumericPages"></PagerStyle>
-    </telerik:RadGrid>
+    <div class="titel2">Overzicht</div>
+    <img id="line" src="Images/loginLine.gif" width="870" height="2" alt="line" />
+    <p class="ptext">Welkom Thomas Hendriksen. U bevindt zich in het overzicht van de door u gereden ritten. Het is mogelijk om informatie over de ritten op te vragen door deze te selecteren en op "ophalen" te klikken.
+     </p>
+    <br />
+    <div id="dataViewLeft">
+        <asp:ListBox CssClass="padding" ID="ListBox1" runat="server" Height="210px" Width="180px"></asp:ListBox>
+        <asp:Button CssClass="paddingTwo" ID="Button1" runat="server" Width="180px"  
+            Text="Ophalen" onclick="Button1_Click" />
+    </div>
+    <div id="dataViewLeftTwo">
+        <p class="padding">
+            <asp:Label ID="Label1" runat="server" Width="180px" Text="Den Haag - Rotterdam" Font-Bold="True"></asp:Label>
+            <br /><br />
+            <asp:Label ID="Label2" runat="server" Width="100px" Text="Datum:"></asp:Label>
+            <asp:Label ID="Label3" runat="server" Text="11-06-2011"></asp:Label>
+            <br /><br />
+            <asp:Label ID="Label4" runat="server" Width="100px" Text="Gem. snelheid:"></asp:Label>
+            <asp:Label ID="Label5" runat="server" Text="leeg"></asp:Label>
+            <br />
+            <asp:Label ID="Label6" runat="server" Width="100px" Text="Top snelheid:"></asp:Label>
+            <asp:Label ID="Label7" runat="server" Text="leeg"></asp:Label>
+            <br /><br /><br />
+            <asp:Label ID="Label8" runat="server" Width="180px" Text="Algemeen" Font-Bold="True"></asp:Label>
+            <br /><br />
+            <asp:Label ID="Label9" runat="server" Width="100px" Text="Gem. snelheid:"></asp:Label>
+            <asp:Label ID="Label10" runat="server" Text="leeg"></asp:Label>
+            <br />
+            <asp:Label ID="Label11" runat="server" Width="100px" Text="Top snelheid:"></asp:Label>
+            <asp:Label ID="Label12" runat="server" Text="leeg"></asp:Label>
+        </p>
+    </div>
+    <div id="dataViewRight">
+        <Web:ChartControl ID="ChartControl1" runat="server" BorderStyle="Outset" 
+            BorderWidth="0" Height="300px" Width="400px" Border-Color="Black" PlotBackground-ForeColor="Black">
+        </Web:ChartControl>
+    </div>
+    <div id="dataViewCenter">
+        <div id="map_canvas"></div>
+    </div>
 </asp:Content>
--- trunk/Car2CloudWebApp/Car2CloudWebApp/Default.aspx.designer.cs
+++ trunk/Car2CloudWebApp/Car2CloudWebApp/Default.aspx.designer.cs
@@ -10,15 +10,150 @@
 namespace ictlab {
     
     
-    public partial class _Default {
+    public partial class Default {
         
         /// <summary>
-        /// Grd1 control.
+        /// js control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::Telerik.Web.UI.RadGrid Grd1;
+        protected global::System.Web.UI.WebControls.Literal js;
+        
+        /// <summary>
+        /// ListBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Label7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+        
+        /// <summary>
+        /// Label8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+        
+        /// <summary>
+        /// Label9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+        
+        /// <summary>
+        /// Label10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+        
+        /// <summary>
+        /// Label11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+        
+        /// <summary>
+        /// Label12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label12;
+        
+        /// <summary>
+        /// ChartControl1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::WebChart.ChartControl ChartControl1;
     }
 }
